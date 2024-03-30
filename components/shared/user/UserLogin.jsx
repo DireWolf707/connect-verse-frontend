bc44b1eeef4c45b672e19febded86b673b0cf4e9
DireWolf707 <rahulsood707@gmail.com>
2024-03-30 22:54:46 +0530

    added github signup page

--- components/shared/user/UserLogin.jsx
+++ components/shared/user/UserLogin.jsx
@@ -2,11 +2,11 @@ import Image from "next/image"
 import Link from "next/link"
 import { Button } from "../../ui/button"
 
-const LoginButton = ({ href, src, alt }) => (
+const LoginButton = ({ text, href, src, alt }) => (
   <Button asChild variant="link" className="bg-white">
     <Link href={href} className="flex gap-2">
       <Image src={src} alt={alt} width={28} height={28} />
-      <span className="font-[600] text-black">Login with Google</span>
+      <span className="font-[600] text-black">{text}</span>
     </Link>
   </Button>
 )
@@ -16,13 +16,15 @@ const UserLogin = () => {
     <div className="flex grow items-center justify-center bg-black">
       <div className="flex flex-col gap-2">
         <LoginButton
+          text="Login with Google"
           href={`${process.env.NEXT_PUBLIC_SERVER_URL}/user/login/google`}
           src="/icons/google.svg"
           alt="google"
         />
 
         <LoginButton
-          href={`${process.env.NEXT_PUBLIC_SERVER_URL}/user/login/google`}
+          text="Login with Github"
+          href={`${process.env.NEXT_PUBLIC_SERVER_URL}/user/login/github`}
           src="/icons/github.svg"
           alt="github"
         />
