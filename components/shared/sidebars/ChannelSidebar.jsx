bb64cce969a04fbdee5ab7dfb8b80d617881af40
DireWolf707 <rahulsood707@gmail.com>
2024-04-29 22:50:46 +0530

    added channel sidebar

--- components/shared/sidebars/ChannelSidebar.jsx
+++ components/shared/sidebars/ChannelSidebar.jsx
@@ -1,7 +1,90 @@
+import { Separator } from "@/components/ui/separator"
+import { useGroup } from "@/state/apis/group"
+import Image from "next/image"
+import { useParams } from "next/navigation"
+import ChannelCard from "../cards/ChannelCard"
+import SpinnerText from "../loading/SpinnerText"
+import CreateChannelModal from "../modals/CreateChannelModal"
+import DeleteGroupModal from "../modals/DeleteGroupModal"
+import ShareGroupModal from "../modals/ShareGroupModal"
+import UpdateGroupModal from "../modals/UpdateGroupModal"
+import UpdateMemberModal from "../modals/UpdateMemberModal"
+import MemberRole from "../user/MemberRole"
+
+const RenderChannels = ({ groupId, channels, type }) => (
+  <div className="flex flex-col gap-1">
+    <div className="flex items-center justify-between">
+      <span className="text-sm font-bold">{type.toUpperCase()} CHANNELS</span>
+
+      <CreateChannelModal groupId={groupId} type={type} />
+    </div>
+
+    <div className="text-main flex flex-col gap-0.5 pl-4 text-sm">
+      {channels.map((channel) => (
+        <ChannelCard key={channel.id} groupId={groupId} channel={channel} />
+      ))}
+    </div>
+  </div>
+)
+
 const ChannelSidebar = () => {
+  const { groupId } = useParams()
+  const { data: group } = useGroup(groupId)
+
   return (
-    <div>
-      <span>ChannelSidebar</span>
+    <div className="flex grow flex-col rounded-tl-xl bg-[#dfe7e8] dark:bg-[#403544]">
+      {group ? (
+        <div className="flex flex-col gap-3 p-4">
+          <div className="flex flex-col items-center gap-4">
+            <div className="flex flex-col items-center">
+              <Image
+                src={group.imageURL}
+                alt="image"
+                height={120}
+                width={120}
+                className="size-[120px] rounded-full p-1"
+              />
+
+              <span className="text-main text-2xl">{group.name}</span>
+
+              <MemberRole role={group.member.role} />
+            </div>
+
+            <div className="flex gap-4">
+              <ShareGroupModal group={group} />
+              {group.member.role === "admin" && (
+                <>
+                  <UpdateMemberModal group={group} />
+                  <UpdateGroupModal group={group} />
+                </>
+              )}
+              <DeleteGroupModal group={group} />
+            </div>
+          </div>
+
+          <Separator className="bg-black dark:bg-white" />
+
+          <RenderChannels
+            groupId={groupId}
+            channels={group.textChannels}
+            type="text"
+          />
+
+          <RenderChannels
+            groupId={groupId}
+            channels={group.audioChannels}
+            type="audio"
+          />
+
+          <RenderChannels
+            groupId={groupId}
+            channels={group.videoChannels}
+            type="video"
+          />
+        </div>
+      ) : (
+        <SpinnerText text="Loading group" />
+      )}
     </div>
   )
 }
