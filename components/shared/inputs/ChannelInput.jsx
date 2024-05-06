eb570b68ee2ecfbf3aaba75894d9ed14a1bfdc11
DireWolf707 <rahulsood707@gmail.com>
2024-05-06 16:07:45 +0530

    added channel input component

---
+++ components/shared/inputs/ChannelInput.jsx
@@ -0,0 +1,49 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { errorHandler } from "@/lib/request_handler"
+import { useCreateMessage } from "@/state/apis/group"
+import { SendHorizonalIcon } from "lucide-react"
+import { useState } from "react"
+import EmojiPickerButton from "../buttons/EmojiPickerButton"
+import ChannelAttachmentUploadModal from "../modals/ChannelAttachmentUploadModal"
+
+const ChannelInput = ({ channel }) => {
+  const [message, setMessage] = useState("")
+  const { mutateAsync: createMessage } = useCreateMessage(
+    channel.groupId,
+    channel.id
+  )
+
+  const handleSendMessage = (e) => {
+    e.preventDefault()
+    createMessage({ content: message }).catch(errorHandler)
+    setMessage("")
+  }
+
+  return (
+    <form
+      onSubmit={handleSendMessage}
+      className="mx-4 mb-3 mt-1.5 flex items-center gap-2 rounded-lg bg-[#dfe7e8] px-3 dark:bg-[#403544]"
+    >
+      <div className="flex grow items-center">
+        <ChannelAttachmentUploadModal channel={channel} />
+        <Input
+          required
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Type Message"
+          className="text-main border-none bg-transparent"
+        />
+        <EmojiPickerButton setMessage={setMessage} />
+      </div>
+      <Button type="submit" variant="ghost" size="link">
+        <SendHorizonalIcon
+          type="submit"
+          className="size-8 cursor-pointer stroke-slate-500 p-1"
+        />
+      </Button>
+    </form>
+  )
+}
+
+export default ChannelInput
---
+++ components/shared/modals/ChannelAttachmentUploadModal.jsx
@@ -0,0 +1,93 @@
+import { Button } from "@/components/ui/button"
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog"
+import { Progress } from "@/components/ui/progress"
+import { getContentType } from "@/lib/utils"
+import { useUploadAttachment } from "@/state/apis/group"
+import { PaperclipIcon } from "lucide-react"
+import Image from "next/image"
+import { useState } from "react"
+import FileUploadButton from "../buttons/FileUploadButton"
+
+const ChannelAttachmentUploadModal = ({ channel }) => {
+  const [open, setOpen] = useState(false)
+  const [file, setFile] = useState(null)
+  const [progress, setProgress] = useState(null)
+
+  const { handler: uploadAttachment, isPending } = useUploadAttachment(
+    channel.groupId,
+    channel.id,
+    setProgress
+  )
+
+  const resetModal = () => {
+    setOpen(false)
+    setProgress(null)
+    setFile((file) => {
+      if (file) URL.revokeObjectURL(file.preview)
+      return null
+    })
+  }
+
+  const fileUploadHandler = () => {
+    if (!file) return
+
+    const formData = new FormData()
+    formData.append("file", file)
+    uploadAttachment(formData).then(resetModal).catch(resetModal)
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <PaperclipIcon className="cursor-pointer stroke-slate-500" />
+      </DialogTrigger>
+
+      <DialogContent className="w-11/12">
+        <span className="text-center font-bold">Add an attachment</span>
+
+        <div className="flex flex-col justify-center gap-4">
+          {file && (
+            <div className="flex flex-col items-center gap-2">
+              {getContentType(file) === "video" ? (
+                <video src={file.preview} alt="media" controls />
+              ) : (
+                <Image
+                  src={file.preview}
+                  alt="media"
+                  height={300}
+                  width={360}
+                  className="max-h-[280px] w-[340px] object-contain"
+                />
+              )}
+              <span className="text-main">{file.name}</span>
+            </div>
+          )}
+
+          {progress ? (
+            <div className="relative">
+              <span className="absolute left-[50%] top-[50%] z-10 translate-x-[-50%] translate-y-[-50%] text-white">
+                {progress}%
+              </span>
+              <Progress value={progress} className="h-8 rounded-none" />
+            </div>
+          ) : (
+            <>
+              <FileUploadButton file={file} setFile={setFile} />
+
+              <Button
+                onClick={fileUploadHandler}
+                type="submit"
+                className="w-full"
+                disabled={!file || isPending}
+              >
+                Upload
+              </Button>
+            </>
+          )}
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default ChannelAttachmentUploadModal
